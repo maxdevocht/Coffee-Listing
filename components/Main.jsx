bc0a1c1882a4447cc37c07e753655b39e1d661bf
Max de Vocht <maxdevocht@hotmail.com>
2024-03-28 17:04:29 +0100

    Update
    Added buttons

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,13 @@ export default function Main() {
                 <p className="header--paragraph">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
             </div>
             <div className="section--list">
-
+                <div className="buttons">
+                    <button className="button">All Products</button>
+                    <button className="button">Available Now</button>
+                </div>
+                <div className="card--list">
+                    <div className="card"></div>
+                </div>
             </div>
         </div>
     )
