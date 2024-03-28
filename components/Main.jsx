a83c5d457132b266d9f6b17e60f121332a6192ac
Max de Vocht <maxdevocht@hotmail.com>
2024-03-28 13:46:17 +0100

    Update css
    Soort van gelukt

---
+++ components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <div className="main--container">
+            <div className="section--header">
+                <h1 className="header--title">Our Collection</h1>
+                <p className="header--paragraph">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+            </div>
+            <div className="section--list">
+
+            </div>
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Banner from '../components/Banner'
+import Main from '../components/Main'
 import './App.css'
 
 export default function App() {
   return (
     <>
-      
+      <Banner />
+      <Main />
     </>
   )
 }
